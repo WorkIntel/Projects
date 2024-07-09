569a2df84e5f8dd87158b94efa6cd752d523a752
UD123 <uri.dubin@gmail.com>
2024-07-09 14:43:01 +0300

    moving noise measurement app

--- Utils/src/measure_camera_noise.py
+++ Utils/src/measure_camera_noise.py
@@ -19,13 +19,15 @@ Install :
 
 import numpy as np
 import cv2 as cv
+import matplotlib.pyplot as plt
+
 
 # local modules
 # importing common Use modules 
-import sys 
-sys.path.append(r'C:\Users\udubin\Documents\Projects\Utils')
 from common import RectSelector
-from opencv_viewer_depth import RealSense
+import sys 
+sys.path.append(r'..\Utils\src')
+from opencv_realsense_camera import RealSense
 
 
 
@@ -46,6 +48,8 @@ class NoiseApp:
         self.img_int_mean = None
         self.img_int_std  = None
 
+        self.show_dict     = {} # hist show
+
         cv.namedWindow('Noise')
         self.rect_sel = RectSelector('Noise', self.on_rect)
 
@@ -55,14 +59,14 @@ class NoiseApp:
 
     def statistics(self, frame):
 
-        ii  = frame < 2**15 # 65535 - None
+        ii  = np.logical_and(frame > 0.5, frame < 15000) # 65535 - None
 
         isok = np.any(np.isfinite([frame[ii]]))
         minv = frame[ii].min()
         maxv = frame[ii].max()
         avg  = frame[ii].mean()
-        stdv  = frame[ii].std()
-        locv = len(frame) - len(ii) #np.any(np.isnan(frame[ii]))
+        stdv = frame[ii].std()
+        locv = ii.sum()  #np.any(np.isnan(frame[ii]))
         print(f'Stat : {minv:.2f}:{avg:.2f}:{maxv:.2f} - std {stdv:.2f}3f. Has None {locv}, is finite {isok}')
 
 
@@ -72,6 +76,7 @@ class NoiseApp:
         self.rect = rect
         self.start_over = True
         print('ud1')
+
 
     def process_image(self, img):
         "makes integration over ROI"
@@ -106,6 +111,36 @@ class NoiseApp:
             vis = draw_str(vis,(x0,y0-10),str(err_std))
 
         return vis
+    
+    def show_histogram(self, img):
+        "show roi histgram"
+        if self.rect is None:
+            print('define ROI')
+            return 0
+        
+        x0, y0, x1, y1 = self.rect
+        img_roi = img[y0:y1,x0:x1].astype(np.float32)
+        # Compute histogram
+        hist, bins = np.histogram(img_roi.flatten(), bins=1024, range=[0, 2**15])
+
+        if not 'fig' in self.show_dict : #len(self.show_dict) < 1:
+            fig, ax = plt.subplots()
+            fig.set_size_inches([24, 16])
+            ax.set_title('Histogram (Depth)')
+            ax.set_xlabel('Bin')
+            ax.set_ylabel('Frequency')
+            lineGray, = ax.plot(bins[:-1], hist, c='k', lw=3)
+            ax.set_xlim(bins[0], bins[-1])
+            ax.set_ylim(0, max(hist)+10)
+            plt.ion()
+            #plt.show()
+
+            self.show_dict = {'fig':fig, 'ax':ax, 'line':lineGray}
+        else:
+            self.show_dict['line'].set_ydata(hist)
+        
+        self.show_dict['fig'].canvas.draw()
+        return
 
     def run(self):
         while True:
@@ -119,8 +154,9 @@ class NoiseApp:
             self.statistics(frame)
             err_std = self.process_image(frame)
 
+            self.show_histogram(frame)
             vis     = self.show_scene(err_std)
-            cv.imshow('Noise (Esc - Exit)', vis)
+            cv.imshow('Noise', vis)
             ch = cv.waitKey(1)
             if ch == ord(' '):
                 self.paused = not self.paused
@@ -131,5 +167,4 @@ class NoiseApp:
 
 if __name__ == '__main__':
     #print(__doc__)
-
     NoiseApp(0).run()
